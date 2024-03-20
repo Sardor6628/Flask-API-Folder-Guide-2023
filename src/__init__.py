c0e32f3f13f38f6222bd1c3c098fec1d4bab2d8b
Sardor6628 <sardor.dsu@gmail.com>
2024-03-20 15:01:11 +0900

    -login -sign-up -get list of all APIs integrated

--- src/__init__.py
+++ src/__init__.py
@@ -32,15 +32,13 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.environ.get("SQLALCHEMY_TRACK_
 # sql alchemy instance
 db = SQLAlchemy(app)
 
-
 # Flask Migrate instance to handle migrations
 migrate = Migrate(app, db)
 
 # import api blueprint to register it with app
 from src.routes import api
+
 app.register_blueprint(api, url_prefix="/api")
 
 # import models to let the migrate tool know
 from src.models.user_model import User
-
-
--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -5,33 +5,25 @@ from datetime import datetime
 import jwt
 import os
 
-# user controller blueprint to be registered with api blueprint
 users = Blueprint("users", __name__)
 
 
-# route for signup api/users/signup
 @users.route('/signup', methods=["POST"])
 def handle_signup():
     try:
-        # first validate required use parameters
         data = request.json
-        if "firstname" in data and "lastname" and data and "email" and "password" in data:
-            # validate if the user exist 
+        if "firstname" in data and "lastname" in data and "email" in data and "password" in data:
             user = User.query.filter_by(email=data["email"]).first()
-            # usecase if the user doesn't exists
             if not user:
-                # creating the user instance of User Model to be stored in DB
                 user_obj = User(
                     firstname=data["firstname"],
                     lastname=data["lastname"],
                     email=data["email"],
-                    # hashing the password
                     password=bcrypt.generate_password_hash(data['password']).decode('utf-8')
                 )
                 db.session.add(user_obj)
                 db.session.commit()
 
-                # lets generate jwt token
                 payload = {
                     'iat': datetime.utcnow(),
                     'user_id': str(user_obj.id).replace('-', ""),
@@ -41,96 +33,73 @@ def handle_signup():
                 }
                 token = jwt.encode(payload, os.getenv('SECRET_KEY'), algorithm='HS256')
                 return Response(
-                    response=json.dumps({'status': "success",
-                                         "message": "User Sign up Successful",
-                                         "token": token}),
+                    response=json.dumps({'status': "success", "message": "User Sign up Successful", "token": token}),
                     status=201,
                     mimetype='application/json'
                 )
             else:
-                print(user)
-                # if user already exists
                 return Response(
                     response=json.dumps({'status': "failed", "message": "User already exists kindly use sign in"}),
                     status=409,
                     mimetype='application/json'
                 )
         else:
-            # if request parameters are not correct 
             return Response(
                 response=json.dumps({'status': "failed",
                                      "message": "User Parameters Firstname, Lastname, Email and Password are required"}),
                 status=400,
                 mimetype='application/json'
             )
-
     except Exception as e:
         return Response(
-            response=json.dumps({'status': "failed",
-                                 "message": "Error Occured",
-                                 "error": str(e)}),
+            response=json.dumps({'status': "failed", "message": "Error Occurred - 1", "error": str(e)}),
             status=500,
             mimetype='application/json'
         )
 
 
-# route for login api/users/signin
 @users.route('/signin', methods=["POST"])
-def handle_login():
+def handle_signin():
     try:
-        # first check user parameters
         data = request.json
-        if "email" and "password" in data:
-            # check db for user records
-            user = User.query.filter_by(email=data["email"]).first()
-
-            # if user records exists we will check user password
-            if user:
-                # check user password
-                if bcrypt.check_password_hash(user.password, data["password"]):
-                    # user password matched, we will generate token
-                    payload = {
-                        'iat': datetime.utcnow(),
-                        'user_id': str(user.id).replace('-', ""),
-                        'firstname': user.firstname,
-                        'lastname': user.lastname,
-                        'email': user.email,
-                    }
-                    token = jwt.encode(payload, os.getenv('SECRET_KEY'), algorithm='HS256')
-                    return Response(
-                        response=json.dumps({'status': "success",
-                                             "message": "User Sign In Successful",
-                                             "token": token}),
-                        status=200,
-                        mimetype='application/json'
-                    )
-
-                else:
-                    return Response(
-                        response=json.dumps({'status': "failed", "message": "User Password Mistmatched"}),
-                        status=401,
-                        mimetype='application/json'
-                    )
-                    # if there is no user record
-            else:
-                return Response(
-                    response=json.dumps({'status': "failed", "message": "User Record doesn't exist, kindly register"}),
-                    status=404,
-                    mimetype='application/json'
-                )
-        else:
-            # if request parameters are not correct 
+        user = User.query.filter_by(email=data["email"]).first()
+        check_validity = not user or not bcrypt.check_password_hash(user.password, data["password"])
+        if check_validity:
+            raise ValueError("Wrong login credentials!")
+        if "email" in data and "password" in data:
             return Response(
-                response=json.dumps({'status': "failed", "message": "User Parameters Email and Password are required"}),
-                status=400,
+                response=json.dumps({'status': "success", "message": "User Sign up Successful", "token": ""}),
+                status=201,
                 mimetype='application/json'
             )
-
+        else:
+            raise ValueError("Email or password is missing")
     except Exception as e:
         return Response(
-            response=json.dumps({'status': "failed",
-                                 "message": "Error Occured",
-                                 "error": str(e)}),
+            response=json.dumps({'status': "failed", "message": "Error Occurred - 1", "error": str(e)}),
+            status=500,
+            mimetype='application/json'
+        )
+
+
+@users.route('/usersAll', methods=["GET"])
+def get_all_users():
+    try:
+        users = User.query.all()
+        list_of_users = []
+        for _user in users:
+            payload = {
+                "user_name": _user.firstname + " " + _user.lastname,
+                "email": _user.email}
+            list_of_users.append(payload)
+        return Response(
+                response=json.dumps(
+                    {'status': "success","users": list_of_users}),
+                status=200,
+                mimetype='application/json')
+    except Exception as e:
+        return Response(
+            response=json.dumps({'status': "failed", "message": "Error Occurred - 1", "error": str(e)}),
             status=500,
             mimetype='application/json'
         )
