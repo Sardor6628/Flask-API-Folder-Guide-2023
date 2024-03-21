78c28bd606d249f0fbdbca835b84befa0ec262be
Sardor6628 <sardor.dsu@gmail.com>
2024-03-21 12:19:00 +0900

    -Task APIs(Update, get user's tasks)

--- src/controllers/task_controller.py
+++ src/controllers/task_controller.py
@@ -13,8 +13,7 @@ def get_users():
         list_of_tasks = []
         query_of_tasks = Task.query.all()
         for _task in query_of_tasks:
-            payload = {"title": _task.title, "body": _task.body}
-            list_of_tasks.append(_task)
+            list_of_tasks.append(_task.to_json())
         return Response(
             response=json.dumps(
                 {'status': "success", "tasks": list_of_tasks}),
@@ -65,6 +64,54 @@ def create():
 @tasks.route('/update', methods=["POST"])
 def update():
     try:
-        pass
+        data = request.json
+        is_right_credentials = "title" in data and "body" in data and "user_id" in data and "id" in data
+        if not is_right_credentials:
+            raise ValueError("Some required parameters are missing")
+        task = Task.query.filter_by(id=data['id']).first()
+        if not task:
+            raise ValueError("There is no task with such id")
+        is_the_same_owner = task.user_id == data['user_id']
+        if not is_the_same_owner:
+            raise ValueError("This task doesn't belong to you")
+        task.body = data['body']
+        task.title = data['title']
+        task.is_modified = True
+        db.session.commit()
+        return Response(
+            response=json.dumps({'status': "success", "message": "Task has been updated!"}),
+            status=200,
+            mimetype='application/json'
+        )
     except Exception as e:
-        pass
+        return Response(
+            response=json.dumps({'status': "failed", "message": "Error Occurred", "error": str(e)}),
+            status=500,
+            mimetype='application/json'
+        )
+
+
+@tasks.route("/get-user-tasks", methods=["POST"])
+def get_user_tasks():
+    try:
+        data = request.json
+        is_right_credentials = "user_id" in data
+        if not is_right_credentials:
+            raise ValueError("Wrong credentials")
+        _user = User.query.filter_by(id=data['user_id']).first()
+        if not _user:
+            raise ValueError("There is no such user")
+        list_of_tasks = Task.query.filter_by(user_id=data['user_id'])
+        payload = []
+        for _task in list_of_tasks:
+            payload.append(_task.to_json())
+        return Response(
+            status=200,
+            mimetype='application/json',
+            response=json.dumps({"status": "success", "message": "returned successfully", "result": payload})
+        )
+    except Exception as e:
+        return Response(
+            response=json.dumps({'status': "failed", "message": "Error Occurred", "error": str(e)}),
+            status=500,
+            mimetype='application/json')
--- src/models/task_model.py
+++ src/models/task_model.py
@@ -8,3 +8,14 @@ class Task(db.Model):
     body = db.Column(db.String(1000))
     created_at = db.Column(db.DATETIME)
     is_modified = db.Column(db.Boolean)
+
+    def to_json(self):
+        created_at_str = self.created_at.strftime('%Y-%m-%d %H:%M:%S') if self.created_at else None
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'title': self.title,
+            'body': self.body,
+            'created_at': created_at_str,
+            'is_modified': self.is_modified
+        }
